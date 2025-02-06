59d187c14e9b10de87da83b869f873e7279c5216
tarunpatil01 <tarunpatil09876@gmail.com>
2025-02-06 12:47:08 +0530

    ...

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-gray-800 text-white text-center p-4">
+    <footer className="bg-gray-800 text-white text-center  p-4">
       <p>© 2021 All Rights Reserved to Kgamify | Version 0.1</p>
     </footer>
   );
