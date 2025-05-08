247d4caa4c67c4bce0e50887e071d1ddf1a204fb
tarunpatil01 <tarunpatil09876@gmail.com>
2025-05-08 23:13:34 +0530

    ....cucu

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://job-portal-backend-629b.onrender.com/api';
+const API_URL = 'https://localhost:5000/api';
 
 export const registerCompany = async (formData) => {
   try {
