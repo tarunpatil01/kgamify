7cc0acc7fa5a093faf91002fca1a3c5f2ca83619
Tarun Patil <119332207+tarunpatil01@users.noreply.github.com>
2025-05-09 01:14:16 +0530

    Update api.js
    backend connect URl api

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://job-portal-backend-629b.onrender.com/api';
 
 export const registerCompany = async (formData) => {
   try {
